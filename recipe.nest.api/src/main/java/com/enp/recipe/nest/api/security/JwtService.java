d9a93af681d2f626ba2dfcfb9f97ebb93cd795e4
eneperezv <eneperezv@gmail.com>
2024-10-21 23:40:39 -0500

    Se incluye documentacion inicial de las clases
     Changes to be committed:
    	modified:   recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/JwtAuthenticationFilter.java
    	modified:   recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/JwtService.java
    	modified:   recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/LoginForm.java
    	modified:   recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/WebSecurityConfig.java

--- recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/JwtService.java
+++ recipe.nest.api/src/main/java/com/enp/recipe/nest/api/security/JwtService.java
@@ -1,5 +1,22 @@
 package com.enp.recipe.nest.api.security;
 
+/*
+ * @(#)JwtService.java 1.0 12/10/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Clase controller para gestion de reservas
+ *
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 import java.time.Instant;
 import java.util.Base64;
 import java.util.Date;
@@ -20,43 +37,43 @@ import io.jsonwebtoken.security.Keys;
 public class JwtService {
 	
 	//KEY FOR AUTHENTICATION
-		//Use generateSecretKey method from de test folder to generate a new key
-		private static final String API_ACCESS = "C43A90EDF68A853BE97FA0890D14961E2D078D8F7AD7F0F3D01EA252C195455F3C612E2CD11F230DB171B732B6BE455E1F480C00D775B069E0A3EEC97EB9526A";
-		private static final long VALIDITY = TimeUnit.MINUTES.toMillis(60);
-		
-		public String generateToken(UserDetails userDetails) {
-			Map<String,String> claims = new HashMap<>();
-			claims.put("iss","bookmaster.api");
-			return Jwts.builder()
-				.claims(claims)
-				.subject(userDetails.getUsername())
-				.issuedAt(Date.from(Instant.now()))
-				.expiration(Date.from(Instant.now().plusMillis(VALIDITY)))
-				.signWith(generateKey())
-				.compact();
-		}
-		
-		private SecretKey generateKey() {
-			byte[] decodedKey = Base64.getDecoder().decode(API_ACCESS);
-			return Keys.hmacShaKeyFor(decodedKey);
-		}
-		
-		public String extractUsername(String jwt) {
-	        Claims claims = getClaims(jwt);
-	        return claims.getSubject();
-	    }
+	//Use generateSecretKey method from de test folder to generate a new key
+	private static final String API_ACCESS = "C43A90EDF68A853BE97FA0890D14961E2D078D8F7AD7F0F3D01EA252C195455F3C612E2CD11F230DB171B732B6BE455E1F480C00D775B069E0A3EEC97EB9526A";
+	private static final long VALIDITY = TimeUnit.MINUTES.toMillis(60);
+	
+	public String generateToken(UserDetails userDetails) {
+		Map<String,String> claims = new HashMap<>();
+		claims.put("iss","bookmaster.api");
+		return Jwts.builder()
+			.claims(claims)
+			.subject(userDetails.getUsername())
+			.issuedAt(Date.from(Instant.now()))
+			.expiration(Date.from(Instant.now().plusMillis(VALIDITY)))
+			.signWith(generateKey())
+			.compact();
+	}
+	
+	private SecretKey generateKey() {
+		byte[] decodedKey = Base64.getDecoder().decode(API_ACCESS);
+		return Keys.hmacShaKeyFor(decodedKey);
+	}
+	
+	public String extractUsername(String jwt) {
+        Claims claims = getClaims(jwt);
+        return claims.getSubject();
+    }
 
-	    private Claims getClaims(String jwt) {
-	        return Jwts.parser()
-	                 .verifyWith(generateKey())
-	                 .build()
-	                 .parseSignedClaims(jwt)
-	                 .getPayload();
-	    }
+    private Claims getClaims(String jwt) {
+        return Jwts.parser()
+                 .verifyWith(generateKey())
+                 .build()
+                 .parseSignedClaims(jwt)
+                 .getPayload();
+    }
 
-	    public boolean isTokenValid(String jwt) {
-	        Claims claims = getClaims(jwt);
-	        return claims.getExpiration().after(Date.from(Instant.now()));
-	    }
+    public boolean isTokenValid(String jwt) {
+        Claims claims = getClaims(jwt);
+        return claims.getExpiration().after(Date.from(Instant.now()));
+    }
 
 }
